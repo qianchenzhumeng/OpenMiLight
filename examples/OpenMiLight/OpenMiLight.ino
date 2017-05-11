39e5ff7de1e0e4126fca431df5d4f611813eefcd
qianchenzhumeng <qianchenzhumeng@live.cn>
2017-05-11 16:20:43 +0800

    Update OpenMiLight.ino

--- examples/OpenMiLight/OpenMiLight.ino
+++ examples/OpenMiLight/OpenMiLight.ino
@@ -15,7 +15,6 @@ OpenMiLight MiLight(mlr);
 void setup()
 {
 	Serial.begin(115200);
-	MiLight.set_id(0xB0, 0x2C, 0x8C);
 	MiLight.begin();
 }
 
@@ -40,29 +39,21 @@ void loop()
 			MiLight.set_pair(1);
 			break;
 		case '2':
-			MiLight.set_color_default(1);
+			MiLight.set_pair(2);
 			break;
 		case '3':
-			MiLight.set_status(1, false);
-			break;
-		case '4':
 			MiLight.set_status(1, true);
 			break;
+		case '4':
+			MiLight.set_status(1, false);
+			break;
 		case '5':
-			MiLight.set_color(0);
+			MiLight.set_status(2, true);
 			break;
 		case '6':
-			MiLight.set_color(50);
+			MiLight.set_status(2, false);
 			break;
-		case '7':
-			MiLight.set_brightness(1);
-			break;
-		case '8':
-			MiLight.set_brightness(50);
-			break;
-		case '9':
-			MiLight.set_brightness(100);
-			break;
+		default:break;
 		}
 	}
 
