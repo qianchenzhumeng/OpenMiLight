875930f42352dbcf1c6a5a5a50079b19e5fff918
qianchenzhumeng <qianchenzhumeng@live.cn>
2017-05-11 16:15:42 +0800

    Update OpenMiLight.h
    Delete some methods of OpenMiLight class.

--- OpenMiLight.h
+++ OpenMiLight.h
@@ -6,15 +6,11 @@
 class OpenMiLight {
 	public:
 		OpenMiLight(MiLightRadio &mlr);
-		void set_id(uint8_t id1, uint8_t id2, uint8_t id3);
 		int begin();
 		void send_command(uint8_t message[6]);
 		void set_pair(uint8_t group);
 		void set_status(uint8_t group, bool status);
-		void set_color(uint8_t red, uint8_t green, uint8_t blue);
-		void set_color(uint8_t color);
 		void set_color_default(uint8_t group);
-		void set_brightness(uint8_t percentage);
 		
 	private:
 		MiLightRadio &_mlr;
